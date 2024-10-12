f215a0498c76200aef2060b6e4afa2360f6943c5
Victor Lelis <ovictorlelis@gmail.com>
2024-10-12 07:16:37 -0300

    add Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.php",
+    "./template-parts/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
